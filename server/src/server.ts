399b52608bf429f75d8690ac41d535ca754adf71
Lucca Passos Covre <luccapassoscovre@gmail.com>
2020-06-02 22:58:03 -0300

    Elaboração do Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-app.get('/users', (req, res) => {
-    res.json(['Vai ser', 'uma jornada'])
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
